859943a7051116278211c0516732c305dc522f6c
hsbhathiya <hsbathiya@gmail.com>
2015-03-09 18:19:39 +0530

    Introduce VirtualMachine

---
+++ azurecompute/src/main/java/org/jclouds/azurecompute/functions/DeploymentToVirtualMachine.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jclouds.azurecompute.functions;
+
+import com.google.common.base.Function;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.jclouds.azurecompute.domain.Deployment;
+import org.jclouds.azurecompute.domain.Role;
+import org.jclouds.azurecompute.domain.VirtualMachine;
+
+import java.util.List;
+import java.util.Map;
+
+public class DeploymentToVirtualMachine implements Function<Deployment, List<VirtualMachine>> {
+
+   @Override
+   public List<VirtualMachine> apply(Deployment input) {
+
+      List<Deployment.RoleInstance> roleInstances = input.roleInstanceList();
+      List<Role> roles = input.roles();
+
+      Map<String, Role> mappedRoles = Maps.uniqueIndex(roles, new Function<Role, String>() {
+               public String apply(Role from) {
+                  return from.roleName(); // or something else
+               }
+            }
+      );
+
+      List<VirtualMachine> virtualMachines = Lists.newArrayList();
+      for (Deployment.RoleInstance instance : roleInstances) {
+
+         VirtualMachine vm = VirtualMachine.builder()
+               .serviceName(instance.hostname())
+               .deploymentName(input.name())
+               .slot(input.slot())
+               .deploymentStatus(input.status())
+               .deploymentLabel(input.label())
+               .roleName(instance.roleName())
+               .instanceSize(instance.instanceSize())
+               .instanceStatus(instance.instanceStatus())
+               .instanceName(instance.instanceName())
+               .instanceErrorCode(input.instanceErrorCode())
+               .instanceStateDetails(input.instanceStateDetails())
+               .instanceEndpoints(instance.instanceEndpoints())
+               .role(mappedRoles.get(instance.roleName()))
+               .virtualNetworkName(input.virtualNetworkName())
+               .build();
+
+         virtualMachines.add(vm);
+      }
+      return virtualMachines;
+   }
+}
---
+++ azurecompute/src/test/java/org/jclouds/azurecompute/functions/DeploymentToVirtualMachineTest.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jclouds.azurecompute.functions;
+
+import org.jclouds.azurecompute.domain.Deployment;
+import org.jclouds.azurecompute.domain.VirtualMachine;
+import org.jclouds.azurecompute.xml.DeploymentHandlerTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.testng.Assert.assertEquals;
+
+@Test(groups = "unit", testName = "DeploymentToVirtualMachineTest")
+public class DeploymentToVirtualMachineTest {
+   public void testDeploymentTransform() {
+      DeploymentToVirtualMachine deploymentToVirtualMachine = new DeploymentToVirtualMachine();
+      Deployment deployment = DeploymentHandlerTest.expected();
+      List<VirtualMachine> transformed = deploymentToVirtualMachine.apply(deployment);
+
+      int i = 0;
+      for (VirtualMachine vm : transformed) {
+         Deployment.RoleInstance instance = deployment.roleInstanceList().get(i);
+         assertEquals(vm.deploymentName(), deployment.name());
+         assertEquals(vm.deploymentLabel(), deployment.label());
+         assertEquals(vm.deploymentStatus(), deployment.status());
+         assertEquals(vm.roleName(), instance.roleName());
+         assertEquals(vm.slot(), deployment.slot());
+         assertEquals(vm.virtualNetworkName(), deployment.virtualNetworkName());
+         assertEquals(vm.instanceSize(), instance.instanceSize());
+         i++;
+      }
+   }
+}
